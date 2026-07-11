15a725b308ad066bdadc3d3926c92b466ba775f7
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-11 19:38:47 +0800

    Feat: add if/else chains for checking if who won

--- javascript.js
+++ javascript.js
@@ -1,29 +1,29 @@
 
 // Function that extracts a random choice of rock, paper, or scissors
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100);
-    if (number > 66) 
-        { return "Rock" } 
-    else if (number > 33) 
-        { return "Paper" } 
-    else 
-        { return "Scissors" }
+  let number = Math.floor(Math.random() * 100);
+  if (number > 66) 
+    { return "Rock" } 
+  else if (number > 33) 
+    { return "Paper" } 
+  else 
+    { return "Scissors" }
 }
 
 // Get input from user and return choice of rock, paper, or scissors
 function getHumanChoice() {
-    let userInput = prompt("Rock, Paper, or Scissors?");
-    if (typeof userInput === "string") 
-        { userInput = userInput.trim().toLowerCase() }
-    switch (userInput) {
-        case "rock":
-            return "Rock";
-        case "paper":
-            return "Paper";
-        case "scissors":
-            return "Scissors";
-        default:
-            return null;
+  let userInput = prompt("Rock, Paper, or Scissors?");
+  if (typeof userInput === "string") 
+    { userInput = userInput.trim().toLowerCase() }
+  switch (userInput) {
+    case "rock":
+      return "Rock";
+    case "paper":
+      return "Paper";
+    case "scissors":
+      return "Scissors";
+    default:
+      return null;
     }
 }
 
@@ -32,17 +32,34 @@ let humanScore = 0;
 let computerScore = 0;
 
 // Round Logic
-function playRound(computerChoice, humanChoice) {
-    switch (computerChoice) {
-        case "Rock":
-            return;
-        case "Paper":
-            return;
-        case "Scissors":
-            return;
-        default:
-            return;
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "Rock") {
+    if (computerChoice === "Rock") {
+      console.log("Tied!")
+    } else if (computerChoice === "Scissors") {
+      console.log("You won! Rock beats Scissors.")
+    } else if (computerChoice === "Paper") {
+      console.log("You lost! Paper beats Rock.")
     }
+  } else if (humanChoice === "Paper") {
+    if (computerChoice === "Paper") {
+      console.log("Tied!")
+    } else if (computerChoice === "Rock") {
+      console.log("You won! Paper beats Rock.")
+    } else if (computerChoice === "Scissors") {
+      console.log("You lost! Scissors beats Paper.")
+    }
+  } else if (humanChoice === "Scissors") {
+    if (computerChoice === "Scissors") {
+      console.log("Tied!")
+    } else if (computerChoice === "Paper") {
+      console.log("You won! Scissors beats Paper.")
+    } else if (computerChoice === "Rock") {
+      console.log("You lost! Rock beats Scissors.")
+    }
+  } else {
+    console.log("You lost! Please enter a valid option.")
+  }
 }
 
 // Convert functions into variables
