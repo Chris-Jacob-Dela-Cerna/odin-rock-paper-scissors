068c31ad9247f1a5504e11f2169f23c3de9e0d0a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-11 17:30:20 +0800

    Feat: add if/else chain for identifying rock, paper or scissors for computer

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 
 // Function that extracts a random choice of rock, paper, or scissors
 function getComputerChoice() {
-    let numberChoice = Math.floor(Math.random() * 100)
-    return numberChoice
+    let number = Math.floor(Math.random() * 100)
+    let computerChoice = undefined
+    if (number > 66) {
+        computerChoice = "Rock"
+    } else if (number > 33) {
+        computerChoice = "Paper"
+    } else {
+        computerChoice = "Scissors"
+    }
+    return computerChoice
 }
 console.log(getComputerChoice())
