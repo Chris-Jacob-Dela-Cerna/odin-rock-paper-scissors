fea0f2cba632db978b6ab892c0624d139d1b6cd6
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-11 17:54:00 +0800

    Feat: change if/else to switch/case for cleaner code

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 // Function that extracts a random choice of rock, paper, or scissors
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100)
+    let number = Math.floor(Math.random() * 100);
     if (number > 66) {
         return "Rock"
     } else if (number > 33) {
@@ -10,22 +10,23 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
-console.log(getComputerChoice())
+console.log(getComputerChoice());
 
 // Get input from user and return choice of rock, paper, or scissors
 function getHumanChoice() {
-    userInput = prompt("Rock, Paper, or Scissors?")
+    let userInput = prompt("Rock, Paper, or Scissors?");
     if (typeof userInput === "string") {
         userInput = userInput.trim().toLowerCase()
     }
-    if (userInput === "rock") {
-        return "Rock"
-    } else if (userInput === "paper") {
-        return "Paper"
-    } else if (userInput === "scissors") {
-        return "Scissors"
-    } else {
-        return "None"
+    switch (userInput) {
+        case "rock":
+            return "Rock";
+        case "paper":
+            return "Paper";
+        case "scissors":
+            return "Scissors"
+        default:
+            return null
     }
 }
-console.log(getHumanChoice())
+console.log(getHumanChoice());
