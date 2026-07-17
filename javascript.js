ec6b077fbc36cbe2b356c7b691142454c353ebae
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-17 14:22:43 +0800

    feat: replace delegation with id matching and remove message variables from playRound

--- javascript.js
+++ javascript.js
@@ -1,17 +1,4 @@
 
-
-// Computer selects from Rock / Paper / Scissors
-function getComputerChoice() {
-  let number = Math.floor(Math.random() * 100);
-  if (number > 66) { 
-    return "Rock" 
-  } else if (number > 33) { 
-    return "Paper" 
-  } else { 
-    return "Scissors" }
-}
-
-
 // Game stat initialization
 let round = 1
 let humanScore = 0;
@@ -20,48 +7,59 @@ let computerScore = 0;
 
 // Round logic
 function playRound(humanChoice, computerChoice) {
-  let roundMessage = `- Round ${round} -\nComputer: ${computerChoice}\nUser:     ${humanChoice}\n- Result -\n`;
-  if (humanChoice === "Rock") {
-    if (computerChoice === "Rock") {
-      roundMessage += "Tied!";
-    } else if (computerChoice === "Scissors") {
-      ++humanScore;
-      roundMessage += "You won! Rock beats Scissors.";
-    } else if (computerChoice === "Paper") {
+  switch (humanChoice) {
+    case (computerChoice):
+      roundResult = "Tied!";
+      break;
+    case ("rock"):
+      if (computerChoice === "paper") {
+        ++computerScore;
+        roundResult = "You lost! Paper beats Rock.";
+      } else if (computerChoice === "scissors") {
+        ++humanScore;
+        roundResult = "You won! Rock beats Scissors.";
+      }
+      break;
+    case ("paper"):
+      if (computerChoice === "rock") {
+        ++humanScore;
+        roundResult = "You won! Paper beats Rock.";
+      } else if (computerChoice === "Scissors") {
+        ++computerScore;
+        roundResult = "You lost! Scissors beats Paper.";
+      }
+      break;
+    case ("scissors"):
+      if (computerChoice === "rock") {
+        ++computerScore;
+        roundResult = "You lost! Rock beats Scissors.";
+      } else if (computerChoice === "paper") {
+        ++humanScore;
+        roundResult = "You won! Scissors beats Paper.";
+      }
+      break;
+    default:
       ++computerScore;
-      roundMessage += "You lost! Paper beats Rock.";
-    }
-  } else if (humanChoice === "Paper") {
-    if (computerChoice === "Paper") {
-      roundMessage += "Tied!";
-    } else if (computerChoice === "Rock") {
-      ++humanScore;
-      roundMessage += "You won! Paper beats Rock.";
-    } else if (computerChoice === "Scissors") {
-      ++computerScore;
-      roundMessage += "You lost! Scissors beats Paper.";
-    }
-  } else if (humanChoice === "Scissors") {
-    if (computerChoice === "Scissors") {
-      roundMessage += "Tied!";
-    } else if (computerChoice === "Paper") {
-      ++humanScore;
-      roundMessage += "You won! Scissors beats Paper.";
-    } else if (computerChoice === "Rock") {
-      ++computerScore
-      roundMessage += "You lost! Rock beats Scissors."
-    }
-  } else {
-    ++computerScore;
-    roundMessage += "You lost! Please enter a valid option.";
+      roundResult = "You lost! Please enter a valid option.";
   }
-  let scoreMessage = `\n- Scores -\nComputer: ${computerScore}\nUser:     ${humanScore}`;
-  console.log(roundMessage + scoreMessage);
-  round++
+  ++round;
+  return roundResult
 }
 
 
-// Round end message
+// Computer selects from Rock / Paper / Scissors
+function getComputerChoice() {
+  const number = Math.floor(Math.random() * 100);
+  if (number > 66) { 
+    return "rock" 
+  } else if (number > 33) { 
+    return "paper" 
+  } else { 
+    return "scissors" }
+}
+
+
+// Round end message TO BE FIXED
 function gameResult() {
   if (humanScore === computerScore) {
     return "--- Tie game! ---";
@@ -73,17 +71,14 @@ function gameResult() {
 }
 
 
-const options = document.getElementById("options")
+const options = document.getElementById("options");
 options.addEventListener("click", function(event) {
-  switch (event.target.id) {
-    case "rock":
-      playRound("Rock", getComputerChoice())
-      break
-    case "paper":
-      playRound("Paper", getComputerChoice())
-      break
-    case "scissors":
-      playRound("Scissors", getComputerChoice())
-      break
-  }
+  const target = event.target
+  if (target.tagName !== "BUTTON") return;
+
+  const playerChoice = target.id,
+        computerChoice = getComputerChoice(),
+        roundResult = playRound(playerChoice, computerChoice);
+
+  console.log(playerChoice, computerChoice, roundResult)
 });
